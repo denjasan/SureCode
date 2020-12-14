886cb1a4b3631bb42845d14168c41aac0db0b831
Denis <denjasan@gmail.com>
2020-12-14 16:05:28 +0300

    1

--- Main.py
+++ Main.py
@@ -3,6 +3,14 @@
 # en: here are the main functions for searching for malicious code elements
 
 
+def main(name='data/files_to_check/input.py', xss_on=True, sqli_on=True):
+    file = open(name, 'r')
+    if xss_on:
+        pass
+    if sqli_on:
+        pass
+
+
 def search(what, file_name):
     """ ru: основная функция поиска по алгоритму Р. Боуера и Д. Мура
     en: the main search function using the algorithm of R. Boyer and J. Moore """
@@ -11,16 +19,20 @@ def search(what, file_name):
             pass
 
 
-def xxs():
+def xxs(name):
     """ ru: XXS уязвимость
     en: XXS vulnerability """
     pass
 
 
-def sql_injection():
+def sql_injection(name):
     """ ru: SQLi уязвимость
     en: SQLi vulnerability """
     pass
+
+
+if __name__ == '__main__':
+    main(name='data/files_to_check/xss&sqli.py')
 
 
 
