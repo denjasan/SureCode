3ce5a858105614b647f2eabdc8ab025fa19960cb
Denis <denjasan@yandex.ru>
2021-01-08 11:06:07 +0300

    Новыой поиск в BM

--- test/test.py
+++ test/test.py
@@ -1,24 +1,26 @@
 import time
+import re
 
 
-# k = 0
-# for i in open('BM.py', 'r').readlines():
-#     if '    ' in i:
-#         k += 1
-#     print(i[:-1])
-# print(k)
+k = 0
+for i in open('BM.py', 'r').readlines():
+    if '    ' in i:
+        k += 1
+    print(i[:-1])
+print(k)
 
-# p = input()
-# s = open('wow.txt', 'r').readlines()
-# before = time.time()
-# res = s.find(p)
-# after = time.time()
-# print(res, after - before)
+p = input()
+s = open('wow.txt', 'r').readlines()
+before = time.time()
+res = 'kek'
+after = time.time()
+print(res, after - before)
 
-# before = time.time()
-# res = sum([len(i) for i in s])
-# after = time.time()
-# print(res, after - before)
+before = time.time()
+res = sum([len(i) for i in s])
+after = time.time()
+print(res, after - before)
+
 
 def my_function(**kid):
     print("His last name is " + kid["lname"])
@@ -26,4 +28,9 @@ def my_function(**kid):
 
 my_function(fname="Tobias", lname="Refsnes")
 
-print('mew')
+print('mew'
+      + 'gav'
+      + 'sex')
+
+print(re.search(r'k\wa', r'kia')[0])
+
